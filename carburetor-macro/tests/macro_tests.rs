456add6466c1da554d439e588ce3f6f6be65beb2
Lim Chun Leng <limchunleng@gmail.com>
2026-02-20 22:00:58 +0900

    chore: remove compile test

--- carburetor-macro/tests/macro_tests.rs
+++
@@ -1,7 +0,0 @@
-#[test]
-#[cfg(feature = "backend")]
-fn backend_tests() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/macro_tests/compile/pass/backend/**/*.rs");
-    t.compile_fail("tests/macro_tests/compile/fail/backend/**/*.rs");
-}
